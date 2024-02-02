41ad35c64fe5944f515fd4626d1923728285e210
SeongHo5 <newfuring1123@gmail.com>
2024-02-02 18:36:32 +0900

    feat: Add time zone setting

--- src/main/java/net/bot/crypto/application/CryptoBotApplication.java
+++ src/main/java/net/bot/crypto/application/CryptoBotApplication.java
@@ -1,8 +1,11 @@
 package net.bot.crypto.application;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -10,5 +13,10 @@ public class CryptoBotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CryptoBotApplication.class, args);
+    }
+
+    @PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 }
